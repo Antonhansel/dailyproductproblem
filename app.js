556a148c7e1c6d34ab68d47eadf32686c78b0ed7
anton <antoninribeaud@gmail.com>
2018-05-01 18:39:40 +0200

    Change mongo url

--- app.js
+++ app.js
@@ -30,8 +30,12 @@ if (!isProduction) {
   app.use(errorhandler())
 }
 
-mongoose.connect('mongodb://localhost/dailyproductproblem')
-mongoose.set('debug', true)
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI)
+} else {
+  mongoose.connect('mongodb://localhost/dailyproductproblem')
+  mongoose.set('debug', true)
+}
 
 require('./models/User')
 require('./config/passport')
